122cb364ed53061dac8509d8c1f16ec5dd6a2b32
nico <nico.campolongo@spanflug.de>
2023-12-01 22:36:51 +0100

    read files manually

--- utils/solution_template.py
+++ utils/solution_template.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from aocd.models import Puzzle
 import utils
 from dataclasses import dataclass
 from pprint import pprint as pp
-from datetime import date
 
 
 @dataclass
@@ -14,12 +12,8 @@ class PlaceHolder:
 
 
 @utils.timeit
-def solve(input_f=None):
-    if not input_f:
-        puzzle = Puzzle(year=2022, day=date.today().day)
-        data = puzzle.input_data.split("\n")
-    else:
-        data = open(input_f).readlines()
+def solve(input_f: str):
+    data = open(input_f).readlines()
     processed_data = []
     for line in data:
         processed_data.append([i for i in line.split()])
