f6125c37c8f7574d5cf5f329c0800a33061c6a45
nico <nico.campolongo@spanflug.de>
2023-12-14 10:16:47 +0100

    adding day 12

--- src/day12/day12.py
+++ src/day12/day12.py
@@ -6,7 +6,7 @@ from functools import lru_cache
 
 
 def is_valid(arr, nums):
-    groups = arr.split('.')
+    groups = arr.split(".")
     seq_lens = [len(g) for g in groups if g]
     if len(seq_lens) != len(nums):
         return False
@@ -16,25 +16,24 @@ def is_valid(arr, nums):
     return True
 
 
-
 @utils.timeit
 def part1(input_f: str):
     data = open(input_f).readlines()
     s = 0
     for line in data:
-        pattern, nums = line.split(' ')
-        nums = [int(i) for i in nums.split(',')]
-        q_marks = pattern.count('?')
-        for p in product('.#', repeat=q_marks):
-            arr = ''
+        pattern, nums = line.split(" ")
+        nums = [int(i) for i in nums.split(",")]
+        q_marks = pattern.count("?")
+        for p in product(".#", repeat=q_marks):
+            arr = ""
             pos = 0
             for i in pattern:
-                if i == '?':
+                if i == "?":
                     arr += p[pos]
                     pos += 1
                 else:
                     arr += i
-            if is_valid(''.join(arr), nums):
+            if is_valid("".join(arr), nums):
                 s += 1
 
     return s
@@ -51,7 +50,7 @@ def get_valid(pattern, pattern_pos, nums, nums_pos, count):
             return 1
         return 0
 
-    if pattern[pattern_pos] == '.':
+    if pattern[pattern_pos] == ".":
         # we can either come from '.' or '#'
         if count == 0:
             # if we come from '.', then count is 0
@@ -62,9 +61,9 @@ def get_valid(pattern, pattern_pos, nums, nums_pos, count):
                 return 0
             else:
                 return get_valid(pattern, pattern_pos + 1, nums, nums_pos + 1, 0)
-    elif pattern[pattern_pos] == '#':
+    elif pattern[pattern_pos] == "#":
         return get_valid(pattern, pattern_pos + 1, nums, nums_pos, count + 1)
-    elif pattern[pattern_pos] == '?':
+    elif pattern[pattern_pos] == "?":
         hash_count = get_valid(pattern, pattern_pos + 1, nums, nums_pos, count + 1)
         if count == 0:
             # if we come from '.', then count is 0
@@ -86,10 +85,10 @@ def part2(input_f: str):
     data = open(input_f).readlines()
     s = 0
     for line in data:
-        pattern, nums = line.split(' ')
-        nums = [int(i) for i in nums.split(',')]
+        pattern, nums = line.split(" ")
+        nums = [int(i) for i in nums.split(",")]
         new_nums = tuple([n for n in nums] * 5)
-        new_pattern = '?'.join([pattern] * 5)
+        new_pattern = "?".join([pattern] * 5)
         ways = get_valid(new_pattern, 0, new_nums, 0, 0)
         # print(ways)
         s += ways
